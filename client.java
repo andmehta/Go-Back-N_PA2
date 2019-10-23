0319019230e7d10cbfc38a205af2f31732f00a16
Andrew <andmehta@icloud.com>
2019-10-22 20:24:54 -0500

    added client

---
+++ client.java
@@ -0,0 +1,26 @@
+/************************
+ *                      *
+ * Andrew Mehta am3258  *
+ * Jake Manning jsm652  *
+ *                      *
+ ************************/
+import java.net.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.io.*;
+
+public class client {
+	
+	/*********************
+	 * Private variables * 
+	 *********************/
+	
+	
+	/*********************
+	 * Private functions * 
+	 *********************/
+	
+	/********************
+	 * Public functions * 
+	 ********************/
+}
